1c7e1c8df2371c94c07d35acc75026b3cd37438c
and <and@hqh.me>
2017-06-20 03:16:32 +0800

    add autoset git config

--- update.sh
+++ update.sh
@@ -4,24 +4,27 @@ THIS_DIR=`dirname $(readlink -f $0)`
 . $THIS_DIR/config.sh
 
 user=$(git config --global --get user.name)
-[ -z $user ] && git config --global --set user.name $USER_NAME
+[ -z $user ] && git config --global --set user.name $GIT_USER_NAME
 
 email=$(git config --global --get user.email)
-[ -z $email ] && git config --global --set user.email $USER_EMAIL
+[ -z $email ] && git config --global --set user.email $GIT_USER_EMAIL
 
 push=$(git config --global --get push.default)
-[ -z $push ] && git config --global --set push.default $PUSH_DEFAULT
+[ -z $push ] && git config --global --set push.default $GIT_PUSH_DEFAULT
 
 push_url=$(git remote get-url --push origin)
 
-if ! echo $push_url | grep -q "${PUSH_USER}@"; then
-	new_url=$(echo $push_url | sed -e "s/\/\//\/\/${PUSH_USER}@/g")
+if ! echo $push_url | grep -q "${GIT_PUSH_USER}@"; then
+	new_url=$(echo $push_url | sed -e "s/\/\//\/\/${GIT_PUSH_USER}@/g")
 	git remote set-url origin $new_url
 	echo "update remote url: $new_url"
 fi
 
+input_msg=$1
+input_msg=${input_msg:="update"}
+
 cd $THIS_DIR
 git add .
-git commit -m "update"
+git commit -m "${input_msg}"
 git push
 
