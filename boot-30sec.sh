0c3b2e6665d128a288dac0f30339703a93171538
and <and@hqh.me>
2017-09-07 20:34:19 +0800

    update

--- boot-30sec.sh
+++ boot-30sec.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-. ./basic_functions.sh
+. $(dirname $(readlink -f $0))/basic_functions.sh
 
 main () 
 {
 	check_sudo
 
+	input_time="30sec"
+	if [ -n "$1" ]; then
+		input_time="$1"
+	fi
+
 	ini_file=/etc/systemd/system/network-online.target.wants/networking.service
 	set_conf $ini_file
-	set_conf TimeoutStartSec 30sec
-	grep TimeoutStartSec $ini_file
+	set_conf TimeoutStartSec $input_time
+	if grep TimeoutStartSec $ini_file; then
+		log 'done!'
+	fi
 }
-
-
-
-
 
 main "$@"; exit $?
 
