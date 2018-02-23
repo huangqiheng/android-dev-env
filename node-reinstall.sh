1164e842abc7a45d6f7ffe5037469874bd9a0534
and <and@hqh.me>
2018-02-24 03:35:54 +0800

    update

---
+++ node-reinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+
+main () 
+{
+	check_sudo
+
+	if [ "$1" = "undo" ]; then
+		apt-get install --reinstall nodejs-legacy     # fix /usr/bin/node
+		n rm 6.0.0     # replace number with version of Node that was installed
+		npm uninstall -g n
+		exit 0
+	fi
+
+	npm cache clean -f
+	npm install -g n
+	n stable
+
+	ln -sf /usr/local/n/versions/node/<VERSION>/bin/node /usr/bin/nodejs
+	n latest
+}
+
+main "$@"; exit $?
+
