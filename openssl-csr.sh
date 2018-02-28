484c71be751b07a8c813843a40317bf90dc285ce
and <and@hqh.me>
2018-03-01 01:33:57 +0800

    update

---
+++ openssl-csr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+
+main () 
+{
+	check_sudo
+
+	if [ "$1" = "" ]; then
+		log 'must input domain name as parmameter'
+		exit 0
+	fi
+
+	sslpath=$THIS_DIR/temp/ssl
+	mkdir -p $sslpath
+
+	keyFile=$sslpath/$1.key
+	csrFile=$sslpath/$1.csr
+
+	if [ ! -f $keyFile]; then
+		openssl genrsa –des3 –out "$keyFile" 2048
+	fi
+
+	openssl req –new –key $keyFile –out $csrFile
+	# https://www.thesslstore.com/blog/generate-2048-bit-csr/ 
+}
+
+main "$@"; exit $?
+
