9ad43a143778d051593cef5f805befdaaf98c9c3
and <and@hqh.me>
2018-03-01 03:48:08 +0800

    update

---
+++ ghost-cli.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+
+main () 
+{
+	check_update
+	setup_nodejs
+
+	npm install -g ghost-cli
+
+	if [ "$1" = "production" ]; then
+		check_apt nginx
+		ufw allow 'Nginx Full'
+
+		check_apt mysql-server
+	fi
+}
+
+setup_nodejs()
+{
+	if command_exists /usr/bin/node; then
+		log "node has been installed"
+		return
+	fi
+
+	curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+	check_apt nodejs
+}
+
+main "$@"; exit $?
