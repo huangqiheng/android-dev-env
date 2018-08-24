671b792dea4f46c22de9ff5fd439c421c248dd4a
and <and@hqh.me>
2018-08-25 01:00:01 +0800

    update

---
+++ mount-fat32.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+
+main () 
+{
+	mount -t vfat -o iocharset=cp936 $1 $2
+	cat <<EOL
+for new device: 
+	mkfs.vfat -o iocharset=cp936 $1 $2
+EOL
+}
+
+main "$@"; exit $?
+
