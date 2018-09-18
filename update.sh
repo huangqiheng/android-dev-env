6a99ee950e94de8eda8412a5bf565ad18c6e6220
and <and@tiandao.io>
2018-09-18 13:45:42 +0800

    update

--- update.sh
+++ update.sh
@@ -6,13 +6,6 @@
 # GIT_PUSH_DEFAULT=simple
 # GIT_PUSH_USER=yougithubaccount
 
-
-# if [ $(whoami) != 'root' ]; then
-# 	echo "This script should be executed as root or with sudo:"
-# 	echo "	sudo $0"
-# 	exit 1
-# fi
-
 THIS_DIR=`dirname $(readlink -f $0)`
 . $THIS_DIR/config.sh
 
