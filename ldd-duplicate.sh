83c5edb330ce337c515e37399a4dc74068f4dd73
and <and@hqh.me>
2018-10-12 07:35:14 +0000

    update

---
+++ ldd-duplicate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+. $THIS_DIR/setup_routines.sh
+
+main () 
+{
+	inputFile="$1"
+	if [ -z $inputFile ]; then
+		show_help_exit
+	fi
+
+	ldd "$inputFile" | grep "not found" | sort -u
+}
+
+maintain()
+{
+	check_update
+	[ "$1" = 'help' ] && show_help_exit $2
+}
+
+show_help_exit()
+{
+	cat << EOL
+
+EOL
+	exit 0
+}
+maintain "$@"; main "$@"; exit $?
