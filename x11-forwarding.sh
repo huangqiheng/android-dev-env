0b2177657e59cdc49b6595b35a67d22f1af0d3c0
and <and@hqh.me>
2018-10-25 10:05:35 +0000

    update

--- x11-forwarding.sh
+++ x11-forwarding.sh
@@ -5,18 +5,37 @@
 
 main () 
 {
-	if [ "$1" = 'client' ];then
-		log 'setting ssh client'
-		x11_forward_client
-		exit
+	num_param=$#
+	if [ $num_param -eq 0 ]; then
+		x11_forward_server
+		exit 0
 	fi
 
-	log 'setting ssh server'
-	x11_forward_server
+
+	if [ "$1" = 'client' ];then
+		x11_forward_client
+		exit 0
+	fi
+
+
+	ssh_target=$1
+	ssh_opt="$2"
+	IFS='@'; set -- "$1"
+	hostName=$2
+
+	if ping -c 1 "$hostName" &> /dev/null; then
+		x11_forward_client
+		ssh -v -X "$ssh_target" "$ssh_opt"
+		exit 0
+	fi
+
+	exit 1
 }
 
 x11_forward_server()
 {
+	check_update
+	log 'setting ssh server'
 	check_apt xauth
 
 	set_conf /etc/ssh/sshd_config
@@ -29,6 +48,8 @@ x11_forward_server()
 
 x11_forward_client()
 {
+	log 'setting ssh client'
+
 	cat > $HOME/.ssh/config <<EOL
 Host *
   ForwardAgent yes
@@ -38,7 +59,6 @@ EOL
 
 maintain()
 {
-	check_update
 	[ "$1" = 'help' ] && show_help_exit $2
 }
 
