087a2b16404dae48bac221c6d01ddf519dbd5cac
and <and@hqh.me>
2018-11-06 00:30:30 +0800

    update

--- astrill-vm.sh
+++ astrill-vm.sh
@@ -14,30 +14,77 @@ main ()
 	check_update_once f
 
 	check_apt xinit ratpoison 
+	check_apt proxychains 
 
 	cloudinit_remove
 	auto_login
 	auto_startx
 
-	check_apt shadowsocks proxychains 
-	ratpoisonrc "exec sslocal $HOME/sslocal.json &"
-
+	install_shadowsocks
 	install_astrill
-	check_apt socat 
-	ratpoisonrc "exec socat tcp-listen:3128,reuseaddr,fork tcp:localhost:3213 &"
+
+	setup_tor '127.0.0.1:7070'
+	setup_socat 3213 3128
+	setup_polipo 7070 8213
 
 	ratpoisonrc "exec Xvfb :1 -screen 0 1920x1080x24+32 -fbdir /var/tmp &"
 	ratpoisonrc "exec DISPLAY=:1 /usr/local/Astrill/astrill"
+	x11_forward_server
 
+	help_text
+}
+
+help_text()
+{
+	cat << EOL
+  astrill: 0.0.0.0:3128
+  ssocks:  0.0.0.0:7070
+  tor:     0.0.0.0:9050
+  http:    0.0.0.0:8213
+EOL
+}
+
+install_shadowsocks()
+{
+	check_apt shadowsocks 
+	ratpoisonrc "exec sslocal $HOME/sslocal.json &"
+}
+
+setup_socat()
+{
+	localport=$1
+	openport=$2
+
+	check_apt socat 
+	ratpoisonrc "exec socat tcp-listen:${openport},reuseaddr,fork tcp:localhost:${localport} &"
+}
+
+setup_tor()
+{
+	socksproxy=$1
 	check_apt tor
+
 	set_conf /etc/tor/torrc
-	set_conf Socks5Proxy '127.0.0.1:7070' ' '
+	set_conf Socks5Proxy "$socksproxy" ' '
 	set_conf SOCKSPort '0.0.0.0:9050' ' '
 	set_conf /etc/tor/torsocks.conf
 	set_conf TorAddress '0.0.0.0'
 	systemctl restart tor
+}
 
-	x11_forward_server
+setup_polipo()
+{
+	socksport=$1
+	webport=$2
+	check_apt polipo
+
+	set_conf '/etc/polipo/config'
+	set_conf socksParentProxy "127.0.0.1:${socksport}"
+	set_conf socksProxyType socks5
+	set_conf proxyAddress '::0'
+	set_conf proxyPort "$webport"
+
+	service polipo restart
 }
 
 fix_gpt_auto_error()
@@ -53,9 +100,7 @@ maintain()
 
 show_help_exit()
 {
-	cat << EOL
-
-EOL
+	help_text
 	exit 0
 }
 maintain "$@"; main "$@"; exit $?
