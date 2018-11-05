fba830c43b720c024649cc35c34943632a029599
and <and@hqh.me>
2018-11-05 23:31:58 +0800

    update

--- polipo-socks2http.sh
+++ polipo-socks2http.sh
@@ -8,17 +8,17 @@ main ()
 	check_apt polipo
 
 	set_conf '/etc/polipo/config'
-	set_conf socksParentProxy '127.0.0.1:3128'
+	set_conf socksParentProxy '127.0.0.1:7070'
 	set_conf socksProxyType socks5
 	set_conf proxyAddress '::0'
-	set_conf proxyPort 8123
+	set_conf proxyPort 8213
 
 	service polipo restart
 	
 	cat << EOL
 trys:
-    npm config set proxy http://127.0.0.1:8123
-    npm config set https-proxy http://127.0.0.1:8123
+    npm config set proxy http://127.0.0.1:8213
+    npm config set https-proxy http://127.0.0.1:8213
 EOL
 }
 
