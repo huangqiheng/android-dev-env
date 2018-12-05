e44ad633f91c5b5040407c5dc0eecff2dca9b026
and <and@hqh.me>
2018-12-05 17:11:40 +0000

    update

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 . $(dirname $(readlink -f $0))/basic_functions.sh
 
-! cmd_exists git && apt install git
-! cmd_exists vim && apt install vim
+! cmd_exists git && apt install -y git
+! cmd_exists vim && apt install -y vim
 
 repo_update
