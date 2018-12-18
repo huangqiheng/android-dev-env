3e6825447625f5cf93bfad8837b9216f7364d1da
and <and@hqh.me>
2018-12-19 01:30:08 +0800

    update

---
+++ audio-reload.sh
@@ -0,0 +1,25 @@
+#!/bin/dash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+. $THIS_DIR/setup_routines.sh
+
+main () 
+{
+	check_sudo
+	pulseaudio -k
+	alsa force-reload
+}
+
+maintain()
+{
+	[ "$1" = 'help' ] && show_help_exit $2
+}
+
+show_help_exit()
+{
+	cat << EOL
+
+EOL
+	exit 0
+}
+maintain "$@"; main "$@"; exit $?
