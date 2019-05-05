e4d6b8b50ddfb93552121f23ca5e1d55840d8d17
and <and@tiandao.io>
2019-05-05 16:51:49 +0800

    update

---
+++ hhkb-gh60.sh
@@ -0,0 +1,67 @@
+#!/bin/dash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+. $THIS_DIR/setup_routines.sh
+
+TARGET=atmega32u4
+VER=
+HEX=$THIS_DIR/data/ok60hhkbgeneral.hex
+# compile .hex file from https://kbfirmware.com/ use data/ok60hhkbgeneral.json
+
+main () 
+{
+	check_apt dfu-programmer
+	get_version
+
+	echo "Erasing..."
+	if [ "$VER" == "0.7" ]
+	then
+		dfu-programmer $TARGET erase --force
+	else
+		dfu-programmer $TARGET erase
+	fi
+	echo Reflashing HEX file...
+	dfu-programmer $TARGET flash "$HEX"
+
+	EXITCODE=$?
+	if [ $EXITCODE -eq 0 ]
+	then
+		echo "Success!"
+	else
+		echo "Fail!"
+	fi
+
+	dfu-programmer $TARGET reset
+	exit $EXITCODE
+}
+
+get_version() 
+{
+	VERINFO=$(dfu-programmer --version 2>&1)
+	if [ $(echo $VERINFO | grep -c "0.6") -gt 0 ]
+	then
+		VER=0.6
+	elif [ $(echo $VERINFO | grep -c "0.7") -gt 0 ]
+	then
+		VER=0.7
+	else
+		echo "dfu-programmer >= 0.6 not installed, please install first."
+		exit 1
+	fi
+}
+
+maintain()
+{
+	check_update
+	[ "$1" = 'help' ] && show_help_exit
+}
+
+show_help_exit()
+{
+	cat << EOL
+
+EOL
+	exit 0
+}
+
+maintain "$@"; main "$@"; exit $?
---
+++ hk-ips.sh
@@ -0,0 +1,49 @@
+#!/bin/dash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+. $THIS_DIR/setup_routines.sh
+
+IFCE=enp1s0
+GWAT=10.0.0.254
+NMSK=255.255.255.0
+
+main () 
+{
+	check_sudo
+	add_ip 1 '10.0.0.23'
+
+	add_ip 2 '59.188.84.62' '255.255.255.248'
+}
+
+add_ip()
+{
+	local index=$1
+	local ip=$2
+	local mask=$3
+
+	if [ "X$mask" = 'X' ]; then
+		mask=$NMSK
+	fi
+	
+	if ifconfig | grep -iq "inet $ip"; then
+		log_y "$ip has added"
+		return 0
+	fi
+	ifconfig $IFCE:$index "$ip" netmask $mask
+}
+
+maintain()
+{
+	check_update
+	[ "$1" = 'help' ] && show_help_exit
+}
+
+show_help_exit()
+{
+	cat << EOL
+
+EOL
+	exit 0
+}
+
+maintain "$@"; main "$@"; exit $?
