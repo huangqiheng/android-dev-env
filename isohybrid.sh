db8528787d8b269475f96ed95cac478abcea457d
and <and@tiandao.io>
2019-05-10 16:04:32 +0800

    update

---
+++ isohybrid.sh
@@ -0,0 +1,45 @@
+#!/bin/dash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+. $THIS_DIR/setup_routines.sh
+
+main () 
+{
+	if [ ! -f "$1" ]; then
+		log_r 'please input iso file'
+		exit 1
+	fi
+
+	if ! ls -l "$2" | grep '^b'; then
+		log_r 'please input device path'
+		exit 1
+	fi
+
+	if ! mount | grep "$2"; then
+		log_r 'please umount some device'
+		exit 1
+	fi
+
+	check_apt syslinux-utils
+
+	cp "$1" /tmp/isohybrid.iso
+	isohybrid /tmp/isohybrid.iso
+	dd if=/tmp/isohybrid.iso of="$2" bs=8M
+}
+
+maintain()
+{
+	check_sudo
+	[ "$1" = 'help' ] && show_help_exit
+}
+
+show_help_exit()
+{
+	cat << EOL
+	sudo iso2hybrid.sh /path/to/image.iso /dev/sdb
+
+EOL
+	exit 0
+}
+
+maintain "$@"; main "$@"; exit $?
