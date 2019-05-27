508e135e667e6ae9b5343517fc6b157a7852032b
and <and@tianddao.io>
2019-05-27 17:39:33 +0800

    update

--- aml-host.sh
+++ aml-host.sh
@@ -5,10 +5,15 @@
 
 main () 
 {
+	check_apt nmap
 
-	for subnet in $(ip addr | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}'); do
-		nmap -sP $subnet | grep "Nmap scan report for aml" 
-	done
+	if [ -z "$1" ]; then
+		for subnet in $(ip addr | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}'); do
+			nmap -sP $subnet | grep "Nmap scan report for aml" 
+		done
+	else 
+		nmap -sP "$1" | grep "Nmap scan report for aml" 
+	fi
 	
 }
 
