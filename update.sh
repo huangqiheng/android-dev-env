4e6b6e185b30914e7494351a66451e0045cd7d2e
and <and@hqh.me>
2019-06-05 13:56:58 +0800

    update

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 . $(dirname $(readlink -f $0))/basic_functions.sh
 
+GIT_USER_NAME=$RUN_USER
+GIT_USER_EMAIL="${GIT_USER_NAME}@github.com"
+
 ! cmd_exists git && apt install -y git
 ! cmd_exists vim && apt install -y vim
 
