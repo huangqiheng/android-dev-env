8e43bc2e9fbe5511030451d4e9b4f5758d8e9e74
and <and@tiandao.io>
2019-06-08 00:09:13 +0800

    update

--- driver-wireless.sh
+++ driver-wireless.sh
@@ -9,10 +9,25 @@ main ()
 	check_apt rtl8812au-dkms
 	
 	# Ralink Technology, Corp. MT7601U Wireless Adapter
+	build_mt7601u
+
+
+}
+
+build_mt7601u()
+{
 	# https://askubuntu.com/questions/457061/ralink-mt7601u-148f7601-wi-fi-adapter-installation
-	check_apt firmware-misc-nonfree
 
+	check_apt linux-headers-generic build-essential git
 
+	cd $CACHE_DIR
+	git clone https://github.com/art567/mt7601usta.git
+	cd mt7601usta/src 
+	make
+	make install
+	mkdir -p /etc/Wireless/RT2870STA/
+	cp RT2870STA.dat /etc/Wireless/RT2870STA/
+	modprobe mt7601Usta
 }
 
 maintain()
