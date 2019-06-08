7db529ea801b71a4a3c8b43a855307313260a16a
and <and@hqh.me>
2019-06-08 14:31:15 +0800

    update

---
+++ iptalbes-clean.sh
@@ -0,0 +1,31 @@
+#!/bin/dash
+
+. $(dirname $(readlink -f $0))/basic_functions.sh
+. $THIS_DIR/setup_routines.sh
+
+main () 
+{
+	iptables -P INPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -P OUTPUT ACCEPT
+	iptables -t nat -F
+	iptables -t mangle -F
+	iptables -F
+	iptables -X
+}
+
+maintain()
+{
+	check_sudo
+	[ "$1" = 'help' ] && show_help_exit
+}
+
+show_help_exit()
+{
+	cat << EOL
+
+EOL
+	exit 0
+}
+
+maintain "$@"; main "$@"; exit $?
