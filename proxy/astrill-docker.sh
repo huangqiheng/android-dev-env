695e5369bf6e4e3a63eefe7cc2e4950cd7e95cf7
and <and@hqh.me>
2019-07-04 07:15:25 +0800

    update

--- proxy/astrill-docker.sh
+++ proxy/astrill-docker.sh
@@ -12,34 +12,75 @@ main ()
 	build_image $astrill_image <<-EOL
 	FROM ubuntu:18.04
 	COPY ./astrill-setup-linux64.deb /root
+
 	RUN apt-get update \\
-	    && apt-get install -y vim net-tools psmisc iproute2 nscd dnsutils \\
-	    && apt-get install -y openssl libssl-dev \\
+	    && apt-get install -y libcap2-bin git gcc make automake libcurl4-gnutls-dev \\
+	    && cd /root && git clone https://github.com/holmium/dnsforwarder.git \\
+	    && cd dnsforwarder && ./configure && make && make install \\
+	    && mkdir -p "/home/$USERNAME/.dnsforwarder" \\
+	    && setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/dnsforwarder \\
+	    && apt-get autoremove
+
+	RUN apt-get install -y openssl libssl-dev \\
 	    && apt-get install -y rng-tools shadowsocks-libev \\
+	    && service shadowsocks-libev stop \\
+	    && apt-get install -y vim net-tools psmisc iproute2 nscd dnsutils \\
 	    && apt-get install -y libgtk2.0-0 libcanberra-gtk-module \\
 	    && apt-get install -y gtk2-engines gtk2-engines-pixbuf gtk2-engines-murrine \\
 	    && apt-get install -y gnome-themes-standard \\
 	    && useradd -m -p \$(openssl passwd -1 $PASSWORD) -s /bin/bash $USERNAME \\
 	    && dpkg -i /root/astrill-setup-linux64.deb \\
-	    && ln -sf /usr/local/Astrill/astrill /usr/local/bin/astrill \\
 	    && apt-get autoremove
+
 	RUN echo '#!/bin/dash' > /usr/local/bin/ss-astrill \\
-	    && echo 'ss-server &' >> /usr/local/bin/ss-astrill \\
-	    && echo 'astrill' >> /usr/local/bin/ss-astrill \\
-	    && chmod a+x /usr/local/bin/ss-astrill
+	    && echo 'ss-tunnel -c /etc/shadowsocks-libev/sstunnel.json -L 8.8.8.8:53 &' >> /usr/local/bin/ss-astrill \\
+	    && echo 'dnsforwarder -D -f /etc/dnsforwarder.conf &' >> /usr/local/bin/ss-astrill \\
+	    && echo 'ss-server -c /etc/shadowsocks-libev/ssserver.json &' >> /usr/local/bin/ss-astrill \\
+	    && echo '/usr/local/Astrill/astrill' >> /usr/local/bin/ss-astrill \\
+	    && chmod a+x /usr/local/bin/ss-astrill \\
+	    && chown $USERNAME:$USERNAME /home/$USERNAME -R
 	USER $USERNAME
 	ENV HOME /home/$USERNAME
 	CMD ss-astrill
 EOL
 
-	cat > /tmp/shadowsocks.conf <<-EOL
+	cat > /tmp/sstunnel.json <<-EOL
 {
-    "server":"0.0.0.0",
-    "server_port":8388,
-    "mode":"tcp_and_udp",
-    "password":"bdzones",
-    "timeout":600,
-    "method":"aes-256-cfb"  	
+        "server":"173.242.116.99",
+        "password":"5dont6share7pass8",
+        "server_port":16666,
+        "mode":"tcp_and_udp",
+	"local_address": "127.0.0.1",
+	"local_port":65353,
+        "method":"xchacha20-ietf-poly1305",
+        "timeout":300,
+        "fast_open":false
+}
+EOL
+
+	cat > /tmp/dnsforwarder.conf <<-EOL
+	LogOn false
+	LogFileThresholdLength 5120000
+	LogFileFolder /var/log
+	UDPLocal 0.0.0.0:53
+	UDPGroup 127.0.0.1:65353 * on
+	BlockNegativeResponse true
+	UseCache true
+	MemoryCache false
+	CacheSize 30720000
+	IgnoreTTL true
+	ReloadCache true
+	OverwriteCache true
+EOL
+
+	cat > /tmp/ssserver.json <<-EOL
+{
+	"server":"0.0.0.0",
+	"server_port":8388,
+	"mode":"tcp_and_udp",
+	"password":"bdzones",
+	"timeout":600,
+	"method":"aes-256-cfb"  	
 }
 EOL
 
@@ -60,7 +101,9 @@ EOL
 			-p "$bindport:8388/udp" \
 			-v /tmp/.X11-unix:/tmp/.X11-unix \
 			-v $HOME/.Xauthority:/home/$USERNAME/.Xauthority \
-			-v /tmp/shadowsocks.conf:/etc/shadowsocks-libev/config.json \
+			-v /tmp/dnsforwarder.conf:/etc/dnsforwarder.conf \
+			-v /tmp/ssserver.json:/etc/shadowsocks-libev/ssserver.json \
+			-v /tmp/sstunnel.json:/etc/shadowsocks-libev/sstunnel.json \
 			--hostname $(hostname) \
 			--name "$contname" $astrill_image
 		exit 0
