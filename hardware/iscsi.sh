d1d20e0e9f644a6d4c7504003280537e32fc3c91
and <and@github.com>
2019-07-17 08:52:44 +0000

    update

---
+++ hardware/iscsi.sh
@@ -0,0 +1,24 @@
+#!/bin/dash
+
+. $(dirname $(dirname $(readlink -f $0)))/basic_functions.sh
+. $ROOT_DIR/setup_routines.sh
+
+main () 
+{
+}
+
+maintain()
+{
+	check_update
+	[ "$1" = 'help' ] && show_help_exit
+}
+
+show_help_exit()
+{
+	cat << EOL
+
+EOL
+	exit 0
+}
+
+maintain "$@"; main "$@"; exit $?
