b438a5592699d3a8bbcfe5c117a27a0d2278f219
and <and@github.com>
2019-10-22 08:14:08 +0000

    update

--- imgiso/ratpoison/1904Armbian.sh
+++ imgiso/ratpoison/1904Armbian.sh
@@ -8,6 +8,7 @@ main ()
 
 	# for system
 	install_ratpoison
+	default_session ratpoison
 	install_pinyin_fcitx
 	install_wallpaper
 	install_xscreensaver
@@ -22,6 +23,21 @@ main ()
 	install_libreoffice
 
 	chownHome
+}
+
+default_session()
+{
+	if [ "$DESKTOP_SESSION" = "$1" ]; then
+		log_y 'session is ready'
+		return
+	fi
+
+	log_y  "Now has sessions: $(ls /usr/share/xsessions/ | sed 's/.desktop//' | tr '\n' ',' | sed 's/,$//')"
+
+	set_conf '/etc/lightdm/lightdm.conf.d/22-armbian-autologin.conf'
+	set_conf user-session "$1"
+
+	log_y  "Set session: $1"
 }
 
 main "$@"; exit $?
