479b2a7bc8a97cfa18fb456929fd492f1568b513
and <and@github.com>
2019-10-23 11:25:55 +0000

    update

--- amlogic/auto-hotspot.sh
+++ amlogic/auto-hotspot.sh
@@ -2,43 +2,17 @@
 
 . $(dirname $(dirname $(readlink -f $0)))/basic_functions.sh
 
+boot_dir=/media/${RUN_USER}/BOOT
 root_dir=/media/${RUN_USER}/ROOTFS
-shadow_file=${root_dir}/etc/shadow
-
 apssid="${APSSID:-ArmbianHotspot}"
-appass="${APPASS:-DontConnectMe}"
-appass="${1:-$appass}"
-
-rootpass="${ROOTPASS:-armbianrootpass}"
-rootpass="${2:-$rootpass}"
+appass="${APPASS:-DontConnectMe}"; appass="${1:-$appass}"
+rootpass="${ROOTPASS:-armbianrootpass}"; rootpass="${2:-$rootpass}"
 
 main () 
 {
-	if [ ! -d $root_dir ]; then
-		log_y 'Please plug in the armbian sd card'
-		exit 1
-	fi
-
-	if [ ! "X$1" = 'X' ]; then
-		rootpass="$1"
-	fi
-
+	check_sdcard
 	auto_login_root "$rootpass"
 	entry_code
-}
-
-auto_login_root()
-{
-	encpass=$(echo "$1" | mkpasswd --method=SHA-512 --stdin)
-	sed -i 's,^\(root:\)[^:]*\(:.*\)$,\1'"$encpass"'\2,' $shadow_file
-
-	rm -f ${root_dir}/root/.not_logged_in_yet
-
-	cat > ${root_dir}/lib/systemd/system/getty@tty1.service.d/20-autologin.conf <<-EOL
-	[Service]
-	ExecStart=
-	ExecStart=-/sbin/agetty --autologin root --noclear %I \$TERM
-EOL
 }
 
 entry_code()
@@ -53,6 +27,55 @@ entry_code()
 	don
 EOL
 
+}
+
+check_sdcard()
+{
+	if [ ! -d $root_dir ]; then
+		check_sudo
+
+		bootPart=$(lsblk -fp | grep "vfat.*BOOT " | head -1 | grep -oP '(/dev/\w*)')
+		rootPart=$(lsblk -fp | grep "ext4.*ROOTFS" | head -1 | grep -oP '(/dev/\w*)')
+		boot_dir=/tmp/${RUN_USER}/BOOT
+		root_dir=/tmp/${RUN_USER}/ROOTFS
+
+		if [ -z $bootPart ]; then
+			log_y 'Not found Armbian SDCARD'
+			exit 1
+		fi
+
+		if [ ! -d $boot_dir/dtb ]; then
+			log_y 'Mount Armbian SDCARD'
+
+			mkdir -p $boot_dir; umount -f $boot_dir >/dev/null 2>&1
+			mkdir -p $root_dir; umount -f $root_dir >/dev/null 2>&1
+			mount $bootPart $boot_dir
+			mount $rootPart $root_dir
+		fi
+	fi
+
+	if [ ! -d $boot_dir/uEnv.ini ]; then
+		log_y 'Please plug in the armbian sd card'
+		exit 2
+	fi
+}
+
+
+
+auto_login_root()
+{
+	encpass=$(echo "$1" | mkpasswd --method=SHA-512 --stdin)
+	shadow_file=${root_dir}/etc/shadow
+
+	sed -i 's,^\(root:\)[^:]*\(:.*\)$,\1'"$encpass"'\2,' $shadow_file
+
+	rm -f ${root_dir}/root/.not_logged_in_yet
+
+	cat > ${root_dir}/lib/systemd/system/getty@tty1.service.d/20-autologin.conf <<-EOL
+	[Service]
+	ExecStart=
+	ExecStart=-/sbin/agetty --autologin root --noclear %I \$TERM
+EOL
 }
 
 main "$@"; exit $?
