d1e9f82fb61ae818e4c439849c90023680f1aefa
and <and@github.com>
2019-11-02 03:05:16 +0800

    update

--- amlogic/armbian-dtb.sh
+++ amlogic/armbian-dtb.sh
@@ -8,8 +8,16 @@ main()
 	check_sdcard
 
 	dtb_file="$1"
+	dtb_rpath=/dtb/amlogic
+
 	if [ "X$1" = 'X' ]; then
-		options=$(ls -1p "$BOOT_PATH/dtb/amlogic")
+		options=$(ls -1p "${BOOT_PATH}${dtb_rpath}")
+
+		if [ "X$options" = 'X' ]; then
+			dtb_rpath=/dtb
+			options=$(ls -1p "${BOOT_PATH}${dtb_rpath}")
+		fi
+
 		#echo "$options" | grep -v / | grep -E '^meson.*.dtb$' | awk '{print NR "> " $s}'
 		echo "$options" | grep -v / | grep -E '^meson.*.dtb$' | nl 
 		echo ''
@@ -17,19 +25,19 @@ main()
 		dtb_file=$(echo "$options" | sed -n "${user_select}p")
 	fi
 
-	if [ ! -f "$BOOT_PATH/dtb/amlogic/$dtb_file" ]; then
+	if [ ! -f "${BOOT_PATH}${dtb_rpath}/$dtb_file" ]; then
 		log_y "Not found dtb file: $dtb_file"
 		exit 1
 	fi
 
 	set_conf "$BOOT_PATH/uEnv.ini"
-	set_conf 'dtb_name' "/dtb/amlogic/$dtb_file"
+	set_conf 'dtb_name' "$dtb_rpath/$dtb_file"
 
 	echo "\n-------------- uEnv.ini -------"
 	cat "$BOOT_PATH/uEnv.ini"
 
 	extconf="$BOOT_PATH/extlinux/extlinux.conf"
-	sed -i 's|\(.*/dtb/amlogic/\).*\.dtb$|\1'"$dtb_file"'|' $extconf
+	sed -i 's|\(.*'"$dtb_rpath"'/\).*\.dtb$|\1'"$dtb_file"'|' $extconf
 
 	echo "\n------------ extlinux.conf -----"
 	cat $extconf |  grep -v -E '^\s*#'
